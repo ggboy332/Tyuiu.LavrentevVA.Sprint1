4b47d6821f0d604afbaa059de2c531bd7cba6208
dimka <dimka556.dim4@gmail.com>
2025-09-19 01:29:21 +0500

    9

--- Tyuiu.LavrentevVA.Sprint1.Task0.V11/Program.cs
+++ Tyuiu.LavrentevVA.Sprint1.Task0.V11/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.LavrentevVA.Sprint1.Task0.V0.Lib;
+using Tyuiu.LavrentevVA.Sprint1.Task0.V11.Lib;
 
 namespace Tyuiu.LavrentevVA.Sprint1.Task0.V11
 {
